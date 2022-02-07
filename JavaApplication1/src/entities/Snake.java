ef253e6f07069091950f29e659c26bf46078e5e4
talalquleh <71414834+talalquleh@users.noreply.github.com>
2022-02-07 13:43:30 +0100

    Update Snake.java

--- JavaApplication1/src/entities/Snake.java
+++ JavaApplication1/src/entities/Snake.java
@@ -115,31 +115,7 @@ public class Snake {
      */
     public int getNumRocksLevel(int level) {
         //levels
-        switch (level) {
-            case 1:
-                return 3;
-            case 2:
-                return 4;
-            case 3:
-                return 5;
-            case 4:
-                return 6;
-            case 5:
-                return 7;
-            case 6:
-                return 8;
-            case 7:
-                return 9;
-            case 8:
-                return 10;
-            case 9:
-                return 11;
-            case 10:
-                return 12;
-            default:
-                break;
-        }
-        return 0;
+        return level+2;
 
     }
 
